7aecbddcd483754d50aef47efe6062ead0850e56
Evgeny Sacharow <eugen.sacharow@gmx.de>
2017-08-18 12:49:45 +0200

    comment CORS headers on server

--- server/server.js
+++ server/server.js
@@ -38,12 +38,12 @@ mongoose.connect(
 /**
  * setup express routes
  */
-// CORS, todo: remove later
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Authorization, Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// CORS, use only for local development!
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Authorization, Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 // Parsers
 app.use(bodyParser.json());
